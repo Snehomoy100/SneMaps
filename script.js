deaa6d79fcc9774e4aa37527d0f3e7cd25636324
snehomoy100 <snehomoy100@gmail.com>
2021-05-02 00:54:47 +0530

    directon feature implemented & updated

---
+++ script.js
@@ -0,0 +1,35 @@
+mapboxgl.accessToken = 'pk.eyJ1Ijoic25laG9tb3kxMDAiLCJhIjoiY2tvNjBnaGY5MWhtZzJxbHl0NWdwOGF0dyJ9.Ps-ZNqoVkpQWZ_apcBgCyA';
+        navigator.geolocation.getCurrentPosition(successLocation, errorLocaion, {
+            enableHighAccuracy: true
+        })
+
+        function successLocation(position) {
+            // console.log(position);
+            pinLocation([position.coords.longitude, position.coords.latitude])
+        };
+
+        function errorLocaion() {
+
+        };
+
+        function pinLocation(center) {
+            const map = new mapboxgl.Map({
+                container: 'map',
+                style: 'mapbox://styles/mapbox/streets-v11',
+                center: center,
+                zoom: 15
+                });
+
+                map.addControl(new mapboxgl.NavigationControl());
+
+                var directions = new MapboxDirections({
+                    accessToken: mapboxgl.accessToken
+                  });
+
+                  map.addControl(directions, 'top-left');
+        };
+
+        
+
+
+        
